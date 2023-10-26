969bb6af235d64d03b7099bc9bd64b60daf9a53b
ibrahimraafatt <139039310+ibrahimraafatt@users.noreply.github.com>
2023-10-26 23:58:13 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * clear_bit - set the value of a bit to 0 at a given index
  * @n: pointer to decimal number to change
  * @index: index position to change
- 
  * Return: 1 if it worked, -1 if error
  */
 
