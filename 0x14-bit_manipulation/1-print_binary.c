35fa60be69514b24bae05d92e8ff7f3fc5fc9279
ibrahimraafatt <139039310+ibrahimraafatt@users.noreply.github.com>
2023-10-26 23:44:53 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-void print_binary(unsigned long int z)
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ 
+ */
+
+void print_binary(unsigned long int n)
 {
 	unsigned long int temp;
 	int shifts;
 
-	if (z == 0)
+	if (n == 0)
 	{
 		printf("0");
 		return;
 	}
 
-	for (temp = z, shifts = 0; (temp >>= 1) > 0; shifts++)
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
 		;
 
 	for (; shifts >= 0; shifts--)
 	{
-		if ((z >> shifts) & 1)
+		if ((n >> shifts) & 1)
 			printf("1");
 		else
 			printf("0");
